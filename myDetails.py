be958a0d6a4f23ea70bd31de5932deb5b3aff9a5
Rajat Soni <rajat4git.01@gmail.com>
2020-09-01 05:07:50 +0530

    Prompt Changed by Dev1

--- myDetails.py
+++ myDetails.py
@@ -3,7 +3,7 @@ myName= input("What is your name? ")
 print(f"Hello, {myName}!")
 
 #Code Added by Dev1:
-print("My pleasure to meet you, "+myName+"!"*10)
+print("Welcome to programming World","!"*10)
 
 #This is just Demo by Dev1
 #------------------------------------------------------
