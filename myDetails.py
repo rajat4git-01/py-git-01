2603a633612e93c74ed31309ce1ee99f40b63ca2
Rajat <rajat4git-02@gmail.com>
2020-09-01 05:23:09 +0530

    Feature added by Dev2

--- myDetails.py
+++ myDetails.py
@@ -7,3 +7,8 @@ print("My pleasure to meet you, "+myName+"!"*10)
 
 #This is just Demo by Dev1
 #------------------------------------------------------
+
+#Code Added by Dev2:
+print("What is your age, {myName}?")
+myAge = input()
+print("You will be",str(int(myAge)), "in a year.")
